0b9a3084363fbaf5334e34066faad93470eeb3ae
phantrongnahn <127733912+phantrongnahn@users.noreply.github.com>
2023-03-13 17:55:52 +0700

    Add files via upload

---
+++ bai2c5/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h \
+ C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h \
+ C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h \
+ C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h
+C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_rcc.h:
+C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/stm32f401re.h:
+C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cm4.h:
+C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/core_cmSimd.h:
+C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/CMSIS/Include/system_stm32f4xx.h:
+C:/Users/DELL/Desktop/SDK_1.0.3_NUCLEO-F401RE\ (1)/SDK_1.0.3_NUCLEO-F401RE/shared/Drivers/STM32F401RE_StdPeriph_Driver/inc/stm32f401re_gpio.h:
